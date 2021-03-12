ea71d69a7dcccddbf26e7c3f80678ee230a928ac
razhman <art.ra2010@yandex.ru>
2021-03-12 19:38:55 +0300

    пофиксены баги

---
+++ public/scripts/main.js
@@ -0,0 +1,54 @@
+function showEngineDescription() {
+  closeNav()
+}
+
+function openNav() {
+  document.getElementById('mySidenav').style.width = '300px'
+  const info = document.getElementById('info')
+  info.onclick = () => {
+      closeNav()
+  }
+}
+
+function closeNav() {
+  document.getElementById('mySidenav').style.width = '0'
+}
+
+function CallPrint(strid) {
+  var prtContent = document.getElementById(strid)
+  var prtCSS = `<style>
+    p {
+        font-weight: bold;
+    }
+    body {
+        width: 100%;
+        grid-template-columns: 65% 35%;
+        grid-template-rows: 60% 40%;
+        font-family: "Open Sans", sans-serif;
+    }
+    </style>`
+  var WinPrint = window.open(
+    '',
+    '',
+    'left=50,top=50,width=800,height=640,toolbar=0,scrollbars=1,status=0'
+  )
+  WinPrint.document.write('<head>')
+  WinPrint.document.write(prtCSS)
+  WinPrint.document.write('</head>')
+  WinPrint.document.write('<body>')
+  WinPrint.document.write(prtContent.innerHTML)
+  WinPrint.document.write('</body>')
+  WinPrint.document.close()
+  WinPrint.focus()
+  WinPrint.print()
+}
+
+function selectionPart() {
+  document.getElementById('info').innerHTML = `<h1>Информация о детали:</h1>
+    <div id="partdesc">
+        <p>Выберите деталь</p>
+    </div>`
+    const viewer = document.getElementById('viewer')
+    viewer.style.height = '920px'
+  closeNav()
+}
