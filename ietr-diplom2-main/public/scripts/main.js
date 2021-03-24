1207baeeefe62c08311e3c84ee8d23ab5464bea6
razhman <art.ra2010@yandex.ru>
2021-03-24 14:24:19 +0300

    изменен интерфейс и пофиксен баг с прокруткой страницы

--- ietr-diplom2-main/public/scripts/main.js
+++ ietr-diplom2-main/public/scripts/main.js
@@ -2,7 +2,7 @@ async function showEngineDescription() {
   $.get('http://localhost:3000/parts', (data) => {
     $('#info').html(data[0].description)
   })
-  closeNav()
+  //closeNav()
 }
 
 function showPartDescription(d) {
@@ -11,107 +11,59 @@ function showPartDescription(d) {
   })
 }
 
-function openNav() {
-  document.getElementById('mySidenav').style.width = '300px'
-  const info = document.getElementById('info')
-  info.onclick = () => {
-    closeNav()
-  }
-}
+// function openNav() {
+//   document.getElementById('mySidenav').style.width = '300px'
+//   const info = document.getElementById('info')
+//   info.onclick = () => {
+//     closeNav()
+//   }
+ 
+// }
 
-function closeNav() {
-  document.getElementById('mySidenav').style.width = '0'
-}
+// function closeNav() {
+//   document.getElementById('mySidenav').style.width = '0'
+// }
 
-function CallPrint(strid) {
-  var prtContent = document.getElementById(strid)
-  var prtCSS = `<style>
-    p {
-        font-weight: bold;
-    }
-    body {
-        width: 100%;
-        grid-template-columns: 65% 35%;
-        grid-template-rows: 60% 40%;
-        font-family: "Open Sans", sans-serif;
-    }
-    </style>`
-  var WinPrint = window.open(
-    '',
-    '',
-    'left=50,top=50,width=800,height=640,toolbar=0,scrollbars=1,status=0'
-  )
-  WinPrint.document.write('<head>')
-  WinPrint.document.write(prtCSS)
-  WinPrint.document.write('</head>')
-  WinPrint.document.write('<body>')
-  WinPrint.document.write(prtContent.innerHTML)
-  WinPrint.document.write('</body>')
-  WinPrint.document.close()
-  WinPrint.focus()
-  WinPrint.print()
-}
+// function CallPrint(strid) {
+//   var prtContent = document.getElementById(strid)
+//   var prtCSS = `<style>
+//     p {
+//         font-weight: bold;
+//     }
+//     body {
+//         width: 100%;
+//         grid-template-columns: 65% 35%;
+//         grid-template-rows: 60% 40%;
+//         font-family: "Open Sans", sans-serif;
+//     }
+//     </style>`
+//   var WinPrint = window.open(
+//     '',
+//     '',
+//     'left=50,top=50,width=800,height=640,toolbar=0,scrollbars=1,status=0'
+//   )
+//   WinPrint.document.write('<head>')
+//   WinPrint.document.write(prtCSS)
+//   WinPrint.document.write('</head>')
+//   WinPrint.document.write('<body>')
+//   WinPrint.document.write(prtContent.innerHTML)
+//   WinPrint.document.write('</body>')
+//   WinPrint.document.close()
+//   WinPrint.focus()
+//   WinPrint.print()
+// }
 
 function selectionPart() {
   document.getElementById('info').innerHTML = `<h1><b>Информация о детали:</b></h1>
     <div id="partdesc">
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        <p>Выберите деталь</p>
-        
+        <p>Выберите деталь</p>      
     </div>`
-  // const viewer = document.getElementById('viewer')
-  // viewer.style.height = '920px'
+  
   closeNav()
 }
 
 function showFirstProcedureDescription() {
-  // document.getElementById('info').innerHTML = '<button type="button" class="btn btn-primary">Начать анимацию</button>'
-  document.getElementById('info').innerHTML = `<p>Выберите деталь</p>
-  <p>Выберите деталь</p>
-  <p>Выберите деталь</p>
-  <p>Выберите деталь</p>
-  <p>Выберите деталь</p>
-  <p>Выберите деталь</p>
- `
-  closeNav()
+   document.getElementById('info').innerHTML = '<button type="button" class="btn btn-primary">Начать анимацию</button>'
+ 
+  //closeNav()
 }
