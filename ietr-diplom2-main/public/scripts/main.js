7c8577d88c9fa995481aa7a0c6365655c391634a
razhman <art.ra2010@yandex.ru>
2021-04-21 15:25:10 +0300

    добавлена диагностика неисправностей(5 пунктов)

--- ietr-diplom2-main/public/scripts/main.js
+++ ietr-diplom2-main/public/scripts/main.js
@@ -1,8 +1,10 @@
-async function showEngineDescription() {
-  $.get('http://localhost:3001/parts2', (data) => {
-    $('#info').html(data[0].description)
-  })
-}
+showProcedures()
+
+// async function showEngineDescription() {
+//   $.get('http://localhost:3001/parts2', (data) => {
+//     $('#info').html(data[0].description)
+//   })
+// }
 
 async function showPartDescription(id) {
   $.get('http://localhost:3001/parts2', (data) => {
@@ -72,7 +74,7 @@ async function showProcedures() {
   $.get('http://localhost:3001/procedures', (data) => {
     let mn = ``
     for (let i = 0; i < data.length; i++) {
-      if (data[i].type == "maintainance") mn += `<li><a href="#" showProcDescr(` + i + `)">` + data[i].name + `</a></li>`;
+      if (data[i].type == "maintainance") mn += `<li><a href="#" onclick = "showProcDescr(` + i + `)">` + data[i].name + `</a></li>`;
       $("#maintainance").html(mn);
     }
   })
